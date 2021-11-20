66c0983686d2f02c7acb539a891cf753d368ab90
Shreyash D <86464303+DeveloperCom@users.noreply.github.com>
2021-11-20 20:29:20 +0530

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-function calcSum(){
-    var one = document.getElementById("one").value;
-    var two = document.getElementById("two").value;
-    var sum = Number(one) + Number(two);
-    document.getElementById("result").value = sum;
-}
